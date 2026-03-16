a51f7b04978ddcf5254e3f2fa815d3adc6a0d89f
Shakeel Mohamed <contact@shakeel.xyz>
2026-03-16 09:36:44 -0700

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import react from 'eslint-plugin-react';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    plugins: {
+      react,
+    },
+    ignores: ['src/test/**', 'src/tests/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'indent': ['error', 4],
+      'react/jsx-indent': ['error', 4],
+      'react/jsx-indent-props': ['error', 4],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
