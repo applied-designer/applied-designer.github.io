4c28c2681116ee484118759239aee50378c4d948
Shakeel Mohamed <contact@shakeel.xyz>
2026-03-18 06:35:44 -0700

    dependency cleanup

--- src/test/quizStyling.test.jsx
+++ src/test/quizStyling.test.jsx
@@ -2,7 +2,6 @@ import { describe, it, expect, beforeEach, afterEach } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import QuizPage from '../pages/Quiz'
-import '@testing-library/jest-dom'
 
 const qCount = 12;
 
